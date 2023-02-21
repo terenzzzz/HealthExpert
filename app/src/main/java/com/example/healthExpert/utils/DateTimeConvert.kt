5e8ef75cedf6dd9c0302c1a48f9e624290782f92
terenzzzz <terence592592@gmail.com>
2023-02-21 18:15:26 +0000

    setup datetime util

---
+++ app/src/main/java/com/example/healthExpert/utils/DateTimeConvert.kt
@@ -0,0 +1,20 @@
+package com.example.healthExpert.utils
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+class DateTimeConvert {
+
+    fun toDate(datetime:Date): String {
+        return SimpleDateFormat("yyyy-MM-dd").format(datetime)
+    }
+
+    fun toTime(datetime:Date): String {
+        return SimpleDateFormat("HH:mm:ss").format(datetime)
+    }
+
+    fun toDateTime(datetime:Date): String {
+        return SimpleDateFormat("YYYY-mm-dd HH:mm:ss").format(datetime)
+    }
+
+}
--- app/src/main/java/com/example/healthExpert/view/home/fragment/History.kt
+++ app/src/main/java/com/example/healthExpert/view/home/fragment/History.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import com.example.healthExpert.compatActivity.HistoryCompatFragment
 import com.example.healthExpert.databinding.FragmentHistoryBinding
 import com.example.healthExpert.utils.DatePickerFragment
+import com.example.healthExpert.utils.DateTimeConvert
 import com.example.healthExpert.viewmodels.UserViewModel
 import java.text.SimpleDateFormat
 import java.time.LocalDate
@@ -17,8 +18,12 @@ import java.util.logging.SimpleFormatter
 
 class History : HistoryCompatFragment() {
     private lateinit var binding: FragmentHistoryBinding
+    private var todayDate = DateTimeConvert().toDate(Date())
 
 
+    override fun onResume() {
+        super.onResume()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,18 +34,17 @@ class History : HistoryCompatFragment() {
         binding.lifecycleOwner = this
         binding.historyViewmodel = historyViewModel
 
-        binding.etDate.text = SimpleDateFormat("yyyy-MM-dd").format(Date())
+        binding.etDate.text = todayDate
 
         binding.etDate.setOnClickListener (View.OnClickListener {
             showDatePickerDialog(binding.root)
         })
 
 
-
         return binding.root
     }
 
-    fun showDatePickerDialog(v: View) {
+    private fun showDatePickerDialog(v: View) {
         val newFragment = DatePickerFragment(binding.etDate)
         newFragment.show(requireActivity().supportFragmentManager, "datePicker")
     }
