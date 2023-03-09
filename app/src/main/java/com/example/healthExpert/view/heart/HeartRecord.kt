2cd71cbf0b497bb77923165746f0bd9a0453cec3
terenzzzz <terence592592@gmail.com>
2023-03-09 00:40:01 +0000

    can use camera

--- app/src/main/java/com/example/healthExpert/view/heart/HeartRecord.kt
+++ app/src/main/java/com/example/healthExpert/view/heart/HeartRecord.kt
@@ -1,22 +1,39 @@
 package com.example.healthExpert.view.heart
 
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
+import android.content.pm.PackageManager
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.*
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
-
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.Surface
+import android.view.TextureView
+import android.view.View
+import android.webkit.PermissionRequest
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.core.content.getSystemService
 import com.example.healthExpert.R
+import java.io.File
 
 
 class HeartRecord : AppCompatActivity() {
+
+    lateinit var captureRequest: CaptureRequest.Builder
+    lateinit var handler: Handler
+    lateinit var handlerThread: HandlerThread
+    lateinit var cameraManager: CameraManager
+    lateinit var textureView: TextureView
+    lateinit var cameraCaptureSession: CameraCaptureSession
+    lateinit var cameraDevice: CameraDevice
+    lateinit var cameraRequest: CaptureRequest
+
+
 
     companion object {
         fun startFn(context: Context) {
@@ -31,23 +48,104 @@ class HeartRecord : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_heart_record)
 
-        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        val heartRateSensor = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE)
+        getPermissions()
 
-        val sensorEventListener = object : SensorEventListener {
-            override fun onSensorChanged(event: SensorEvent?) {
-                if (event?.sensor?.type == Sensor.TYPE_HEART_RATE) {
-                    val heartRate = event.values[0]
-                    val heartRateTextView = findViewById<TextView>(R.id.heartRate)
-                    heartRateTextView.text = "Heart Rate: $heartRate bpm"
-                    Log.d("心率", "onSensorChanged: ${heartRate}")
-                }
+        textureView = findViewById(R.id.texture_view)
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        handlerThread = HandlerThread("videoThread")
+        handlerThread.start()
+        handler = Handler((handlerThread).looper)
+
+        textureView.surfaceTextureListener = object: TextureView.SurfaceTextureListener{
+            override fun onSurfaceTextureAvailable(p0: SurfaceTexture, p1: Int, p2: Int) {
+                openCamera()
             }
-            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
+            override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture, p1: Int, p2: Int) {
+            }
+
+            override fun onSurfaceTextureDestroyed(p0: SurfaceTexture): Boolean {
+                return false
+            }
+
+            override fun onSurfaceTextureUpdated(p0: SurfaceTexture) {
+            }
+
         }
 
-        sensorManager.registerListener(sensorEventListener, heartRateSensor, SensorManager.SENSOR_DELAY_NORMAL)
-
-
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraDevice.close()
+        handler.removeCallbacksAndMessages(null)
+        handlerThread.quitSafely()
+    }
+
+    @SuppressLint("MissingPermission")
+    fun openCamera(){
+        cameraManager.openCamera(cameraManager.cameraIdList[0],object : CameraDevice.StateCallback(){
+            override fun onOpened(p0: CameraDevice) {
+                cameraDevice = p0
+
+                captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                var surface = Surface(textureView.surfaceTexture)
+                captureRequest.set(CaptureRequest.FLASH_MODE, CaptureRequest.FLASH_MODE_TORCH)
+                captureRequest.addTarget(surface)
+
+                cameraDevice.createCaptureSession(listOf(surface),object : CameraCaptureSession.StateCallback(){
+                    override fun onConfigured(p0: CameraCaptureSession) {
+                        cameraCaptureSession = p0
+                        cameraCaptureSession.setRepeatingRequest(captureRequest.build(),null,null)
+                    }
+
+                    override fun onConfigureFailed(p0: CameraCaptureSession) {
+
+                    }
+
+                },handler)
+            }
+
+            override fun onDisconnected(p0: CameraDevice) {
+
+            }
+
+            override fun onError(p0: CameraDevice, p1: Int) {
+
+            }
+
+        },handler)
+    }
+
+
+    private fun getPermissions(){
+        var permissionList = mutableListOf<String>()
+        if(checkSelfPermission(android.Manifest.permission.CAMERA)!=PackageManager.PERMISSION_GRANTED)
+            permissionList.add(android.Manifest.permission.CAMERA)
+        if(checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED)
+            permissionList.add(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        if(checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED)
+            permissionList.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        if (permissionList.size > 0){
+            requestPermissions(permissionList.toTypedArray(),10)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        grantResults.forEach {
+            if (it != PackageManager.PERMISSION_GRANTED){
+                getPermissions()
+            }
+        }
+    }
+
+
+
 }
+
