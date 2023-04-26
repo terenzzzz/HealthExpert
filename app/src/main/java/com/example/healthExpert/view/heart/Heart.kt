9c7480d986cd9e788b97c5e999095c36a68f7f75
terenzzzz <terence592592@gmail.com>
2023-04-26 08:12:28 +0100

    linechart for heartrate

--- app/src/main/java/com/example/healthExpert/view/heart/Heart.kt
+++ app/src/main/java/com/example/healthExpert/view/heart/Heart.kt
@@ -24,6 +24,7 @@ import com.example.healthExpert.utils.DateTimeConvert
 import com.example.healthExpert.view.calories.CaloriesAdapter
 import com.example.healthExpert.view.calories.CaloriesEdit
 import com.example.healthExpert.widget.Ring
+import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.components.XAxis
@@ -31,14 +32,17 @@ import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.formatter.ValueFormatter
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.logging.SimpleFormatter
 import kotlin.collections.ArrayList
+import kotlin.math.log
 
 
 class Heart : HeartRateCompatActivity() {
     private lateinit var binding: ActivityHeartBinding
+    private lateinit var lineChart: LineChart
     private var todayDate = DateTimeConvert.toDate(Date())
     private lateinit var recyclerView: RecyclerView
     private lateinit var layoutManager: LinearLayoutManager
@@ -58,7 +62,7 @@ class Heart : HeartRateCompatActivity() {
         setContentView(binding.root)
         binding.dateTime.text = todayDate
 
-        heartSetUp(binding.heartChart)
+        lineChart = heartSetUp(binding.heartChart)
         recyclerView = findViewById (R.id.recycler_view)
         layoutManager = LinearLayoutManager(this)
         layoutManager.reverseLayout = true
@@ -69,17 +73,43 @@ class Heart : HeartRateCompatActivity() {
         heartRateViewModel.heartRates.observe(this, Observer { heartRates ->
             // Update the UI based on the value of MutableLiveData
             if (heartRates != null &&  heartRates.isNotEmpty()) {
-                Log.d("测试", "item: ${heartRates[0].HeartRate}")
                 recyclerView.adapter = HeartRateAdapter(heartRateViewModel.heartRates,this)
 
                 binding.avgBpm.text = String.format("%.0f", heartRates.map { it.HeartRate.toInt() }.average()) + " BPM"
                 binding.maxBpm.text = "${heartRates.maxBy { it.HeartRate }.HeartRate} BPM"
+
+                // Chart
+                val list: MutableList<Entry> = ArrayList()
+                for (i in 0 until 24) {
+                    list.add(Entry(i.toFloat(),0f))
+                }
+                for (hr in heartRates){
+                    val hour = SimpleDateFormat("HH").format(hr.Date)
+                    if (list[hour.toInt()].y != 0f){
+                        list[hour.toInt()].y = (list[hour.toInt()].y + hr.HeartRate.toFloat())/2
+                    }else{
+                        list[hour.toInt()].y = hr.HeartRate.toFloat()
+                    }
+                }
+
+                val lineDataSet = LineDataSet(list, "123")
+                val lineData = LineData(lineDataSet)
+
+                lineDataSet.setDrawValues(true)
+
+                lineChart.data = lineData
+                lineDataSet.setColor(Color.rgb(255,82,82)) //折线的颜色
+                lineDataSet.setLineWidth(4F)     //折线的粗细
+                lineDataSet.setCircleRadius(0F)      //圆点的半径
+                lineDataSet.setCircleColor(Color.rgb(255,82,82))
+                lineDataSet.setDrawCircleHole(false) //false表示直接画成实心圆
+
             }
         })
 
-        binding.addBtn.setOnClickListener (View.OnClickListener { view ->
+        binding.addBtn.setOnClickListener {
             HeartRecord.startFn(this)
-        })
+        }
 
         binding.backBtn.setOnClickListener (View.OnClickListener { view ->
             finish()
@@ -92,48 +122,46 @@ class Heart : HeartRateCompatActivity() {
     override fun onResume() {
         super.onResume()
         heartRateViewModel.getHeartRates(todayDate)
+
     }
 
-    private fun heartSetUp(view: View){
+    private fun heartSetUp(view: View): LineChart {
         // Find View
         val lineChart = view.findViewById<LineChart>(R.id.heartChart)
         // Init data
         val list: MutableList<Entry> = ArrayList()
-        list.add(Entry(0f,60f))
-        list.add(Entry(1f,78f))
-        list.add(Entry(2f,80f))
-        list.add(Entry(3f,72f))
-        list.add(Entry(4f,94f))
-        list.add(Entry(5f,85f))
-        list.add(Entry(6f,62f))
-
-        // Create a list of x-axis labels
-        val xAxisLabels = listOf("00", "04", "08", "12", "16", "20", "24")
+        for (i in 0 until 24) {
+            list.add(Entry(i.toFloat(),0f))
+        }
 
         // Add data to Chart
         val lineDataSet = LineDataSet(list, "123")
         val lineData = LineData(lineDataSet)
+        lineChart.data = lineData
 
         // Chart Setting
-        lineChart.data = lineData
-        lineChart.getXAxis().setDrawGridLines(false)  //是否绘制X轴上的网格线（背景里面的竖线）
+//        lineChart.getXAxis().setDrawGridLines(false)  //是否绘制X轴上的网格线（背景里面的竖线）
         lineChart.getDescription().setEnabled(false)  //是否显示右下角描述
         lineChart.setTouchEnabled(false) // 禁止互动
+        lineChart.getAxisRight().setEnabled(false)
+
         val legend: Legend = lineChart.getLegend()
         legend.isEnabled = false //是否显示图例
         val xAxis: XAxis = lineChart.getXAxis()
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM)        //X轴所在位置   默认为上面
-        xAxis.valueFormatter = IndexAxisValueFormatter(xAxisLabels)
-        xAxis.setAxisMaximum(lineChart.xChartMax+1f);
-        lineChart.getAxisRight().setEnabled(false)
+
+
+
         lineDataSet.setColor(Color.rgb(255,82,82)) //折线的颜色
         lineDataSet.setLineWidth(4F)     //折线的粗细
         lineDataSet.setCircleRadius(0F)      //圆点的半径
-        lineDataSet.setCircleColor(Color.BLACK)
+        lineDataSet.setCircleColor(Color.rgb(255,82,82))
         lineDataSet.setDrawCircleHole(false) //false表示直接画成实心圆
 
         lineChart.animateXY(1000, 1000);
         lineChart.invalidate() // 刷新
+
+        return lineChart
     }
 }
 
